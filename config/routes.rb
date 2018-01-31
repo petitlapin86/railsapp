b3cb9b92d26ab996555c1e6b3b0f82fa4c2827a9
petitlapin86 <skinnerpa86@yahoo.com>
2018-01-31 12:47:23 -0500

    adding nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  resources :posts
-
+  resources :posts do
+    resources :comments
+    #BELOW IS THE MANUAL EQUIVALENT TO THE SHORTCUT :comments
+    #get '/comments', to: 'comments#index', as: 'comments'
+    #post '/comments', to: 'comments#create'
+    #get '/comments/new', to: 'comments#new', as: 'new_comment'
+    #get '/comments/:id', to: 'comments#show', as: 'comment'
+    #get '/comments/:id/edit', to: 'comments#edit', as: 'edit_comment'
+    #patch '/comments/:id', to: 'comments#update'
+    #delete '/comments/:id', to: 'comments#destroy'
+  end
   resources :pages
   #BELOW IS THE MANUAL EQUIVALENT TO THE SHORTCUT :pages ABOVE:
 
