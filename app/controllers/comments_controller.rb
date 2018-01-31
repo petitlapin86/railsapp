71f844cf86b4f36889adfa8e7a984f13a965aeca
petitlapin86 <skinnerpa86@yahoo.com>
2018-01-31 14:25:39 -0500

    updating an associated record i.e comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,28 @@
 class CommentsController < ApplicationController
+before_action :set_post
+
   def create
-    @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
+    redirect_to @post
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params)
     redirect_to @post 
   end
+
   private
   def comment_params
     params.require(:comment).permit(:name, :content)
   end
+
+  def set_post
+        @post = Post.find(params[:post_id])
+  end
+
 end
